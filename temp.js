68a71d2ed123e491274395e394ff68fa9d4ca13e
sjtwgh <sh1j1n9ta0@foxmail.com>
2023-09-06 02:19:20 +0800

    重大更新
    1.重命名data.json为characters.json，并添加夏活2期&尼尔联动角色
    2.于dotgg.gg/nikke处获取到充速表，补齐新角色充速信息
    3.其他优化与修复bug

--- temp.js
+++ temp.js
@@ -1,13 +1,26 @@
-let str = 
-`{
-    "Name":"${$('body > main > h1').text()}",
-    "ID":"${$('body > main > div.contentbox.character_header__lVZI2 > img').attr('src').match(/c(\d{3})?_/)[1]}",
-    "Manufacturer":"${$('body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(1) > div:nth-child(2)').text()}",
-    "Squad":"${$('body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(2) > div:nth-child(2)').text()}",
-    "Weapon":"${$('body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(3) > div:nth-child(2)').text()}",
-    "Class":"${$('body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(4) > div:nth-child(2)').text()}",
-    "Element":"${$('body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(5) > div:nth-child(2)').text()}",
-    "Burst":"${$('body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(6) > div:nth-child(2)').text()}",
+let str = `{
+    "Name":"${$("body > main > h1").text()}",
+    "ID":"${
+        $("body > main > div.contentbox.character_header__lVZI2 > img")
+            .attr("src")
+            .match(/c(\d{3})?_/)[1]
+    }",
+    "Manufacturer":"${$("body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(1) > div:nth-child(2)").text()}",
+    "Squad":"${$("body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(2) > div:nth-child(2)").text()}",
+    "Weapon":"${$("body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(3) > div:nth-child(2)").text()}",
+    "Class":"${$("body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(4) > div:nth-child(2)").text()}",
+    "Element":"${$("body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(5) > div:nth-child(2)").text()}",
+    "Burst":"${$("body > main > div.contentbox.character_header__lVZI2 > div > ul > li:nth-child(6) > div:nth-child(2)").text()}",
     "Charging":""
-},`
+},`;
 console.log(str);
+
+// 获取充速表
+let wc = {};
+$("body > main > table > tbody")
+    .children()
+    .each(function () {
+        let nikke = $(this).find("span,td:nth-child(3)");
+        wc[$(nikke[0]).text()] = $(nikke[1]).text();
+    });
+console.log(JSON.stringify(wc));
